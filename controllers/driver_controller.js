df7744840b8015486071947cdaaac11b595602a8
Amit Ranan <ranan.amit@gmail.com>
2017-01-22 21:55:51 -0800

    created create drivers route and defined mthod on controller

--- controllers/driver_controller.js
+++ controllers/driver_controller.js
@@ -1,5 +1,9 @@
 module.exports = {
   greeting(req, res) {
     res.send({ hi: 'there'});
+  },
+
+  create(req, res) {
+
   }
 };
--- routes/routes.js
+++ routes/routes.js
@@ -2,4 +2,6 @@ const DriversController = require('../controllers/driver_controller');
 
 module.exports = (app) => {
   app.get('/api', DriversController.greeting);
+
+  app.post('/api/drivers', DriversController.create);
 }
