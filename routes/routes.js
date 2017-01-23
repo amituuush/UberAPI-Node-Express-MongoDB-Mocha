082ae9c1ab13196161aa06c01bbf8b22e883246f
Amit Ranan <ranan.amit@gmail.com>
2017-01-22 23:38:10 -0800

    created delete route for /api/drivers/:id

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const DriversController = require('../controllers/drivers_controller');
 
 module.exports = (app) => {
   app.get('/api', DriversController.greeting);
-
   app.post('/api/drivers', DriversController.create);
   app.put('/api/drivers/:id', DriversController.edit);
+  app.delete('/api/drivers/:id', DriversController.delete);
 }
