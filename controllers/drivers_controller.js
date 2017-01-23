e7fc9cd5e7f2687ee5d566fb1eb06d518bfbacda
Amit Ranan <ranan.amit@gmail.com>
2017-01-23 13:23:52 -0800

    added .then and .catch to index handler

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -11,6 +11,8 @@ module.exports = {
       { type: 'Point', coordinates: [lng, lat] },
       { spherical: true, maxDistance: 200000 }
     )
+      .then(drivers => res.send(drivers))
+      .catch(next);
   },
 
   create(req, res, next) {
