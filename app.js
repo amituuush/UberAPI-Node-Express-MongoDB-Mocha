9e7ae628f8cd8f04186332e6a31585967c77b386
Amit Ranan <ranan.amit@gmail.com>
2017-01-22 21:01:42 -0800

    add get route to /api

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.get('/api', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
 module.exports = app;
--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const app = require('./app');
 
 app.listen(3050, () => {
   console.log('Running on port 3050');
-})
+});
